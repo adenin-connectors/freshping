206728a4b28c07420a7907a06baf39184f6b3912
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-07 13:56:14 +0200

    Added new activity 'server-status.js'.

--- activities/state-handler.js
+++ activities/state-handler.js
@@ -2,20 +2,26 @@
 module.exports = async (activity) => {
   try {
     const request = activity.Request.Data;
-    if(!request.webhook_event_data) return;
-    if (request.webhook_event_data.check_state_name != "Available") return;
+    if (!request.webhook_event_data) return;
 
     let entity = {};
     let collections = [];
+    let roles = [];
+
     let date = new Date(request.webhook_event_data.request_start_time).toISOString();
     entity = {
-      _type: request.organization_name,
+      _type: "server-status",
       id: "" + request.organization_name,
       title: request.webhook_event_data.check_name,
       description: request.webhook_event_data.check_state_name,
       date: date,
+      link: request.webhook_event_data.request_url
     };
-    let roles = activity.Context.connector.custom1.split(",").map(role => role.trim());
+    
+    if (request.webhook_event_data.check_state_name != "Available"){
+      roles = activity.Context.connector.custom1.split(",").map(role => role.trim());
+    }
+   
     collections.push({ name: "open", users: [], roles: roles, date: date });
 
     activity.Response.Data = { entity: entity, collections: collections };
