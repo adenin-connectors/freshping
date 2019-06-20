e91a9438e1e66730b025bc9046bfbe7a7ca83d3a
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-06-20 09:37:19 +0200

    Added date to status interface.

--- activities/server-status.js
+++ activities/server-status.js
@@ -13,19 +13,22 @@ module.exports = async function (activity) {
     if ($.isErrorResponse(activity, response)) return;
 
     activity.Response.Data.items = response.body.Data.items;
-    let value = response.body.Data.count;
-    activity.Response.Data.title = T(activity, 'Servers Down');
-    activity.Response.Data.link = "";
-    activity.Response.Data.linkLabel = T(activity, 'All Servers That Are Down');
-    activity.Response.Data.actionable = value > 0;
+    if (parseInt(pagination.page) == 1) {
+      let value = response.body.Data.count;
+      activity.Response.Data.title = T(activity, 'Servers Down');
+      activity.Response.Data.link = "";
+      activity.Response.Data.linkLabel = T(activity, 'All Servers That Are Down');
+      activity.Response.Data.actionable = value > 0;
 
-    if (value > 0) {
-      activity.Response.Data.value = value;
-      activity.Response.Data.color = 'blue';
-      activity.Response.Data.description = value > 1 ? T(activity, "{0} servers are currently down.", value)
-        : T(activity, "1 server is currently down.");
-    } else {
-      activity.Response.Data.description = T(activity, 'All servers are running.');
+      if (value > 0) {
+        activity.Response.Data.value = value;
+        activity.Response.Data.date = response.body.Data.items[0].date;
+        activity.Response.Data.color = 'blue';
+        activity.Response.Data.description = value > 1 ? T(activity, "{0} servers are currently down.", value)
+          : T(activity, "1 server is currently down.");
+      } else {
+        activity.Response.Data.description = T(activity, 'All servers are running.');
+      }
     }
   } catch (error) {
     $.handleError(activity, error);
